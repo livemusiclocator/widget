0a5f5932afe988cfd7819df56de69d099f4352a0
bringthepunters <155452570+bringthepunters@users.noreply.github.com>
2024-11-16 15:42:34 +1100

    pkj

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/widget-maker/', // Update this to match your repository name
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/widget/',
 });
