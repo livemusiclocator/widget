d56f587bf0d555f9b999356f691c5912b908c977
bringthepunters <155452570+bringthepunters@users.noreply.github.com>
2024-11-16 15:58:17 +1100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lml.live/widget/',
+  base: '/widget/',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
